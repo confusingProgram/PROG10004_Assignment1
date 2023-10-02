8a5fdfd3d5e9d0aef600be4866b8b74bb047a45f
Andy Do <doandy@sheridancollege.ca>
2023-10-02 16:19:35 -0400

    Added a confirmation question for role selection

--- App.py
+++ App.py
@@ -3,19 +3,32 @@ print("Your goal is to make a delivery to 123 Somewhere Street.")
 print("The two roles are either a pizza delivery driver, or mail courier.")
 
 role = 0
+name = ""
+choice = ""
 while True:
-    role = input("Please enter 1 for pizza delivery driver, or 2 for mail courier.")
+    role = input("Please enter 1 for pizza delivery driver, or 2 for mail courier: ")
     
     if role.isdigit():
         role = int(role)
-        if role == 1 or role == 2:
-            break
+        if role == 1 or role == 2: #checks if role is 1 or 2
+            if role == 1: #assigns name
+                name = "pizza delivery driver"
+            else:
+                name = "mail courier"
+            
+            while True:
+                choice = input("Are you sure you want to play as " +name + "? Yes or No: ")
+                if choice == "Yes" or choice == "No":
+                    break
+                else:
+                    print("Error, invalid answer.")
+
+            if choice == "Yes":
+                print("Great! You have chosen to play as the " +name +"!")
+                break
         else:
             print("Error, invalid answer.")
     else:
-        ("Error, invalid answer.")
+        print("Error, invalid answer.")
 
-if role == 1:
-    print("Great! You have chosen to play as the pizza delivery driver!")
-else:
-    print("Great! You have chosen to play as the mail courier!")
+print("here")
