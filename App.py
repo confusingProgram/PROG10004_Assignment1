9d970619be7faebf48ff717267e9fa5f4b73e2cf
Andy Do <doandy@sheridancollege.ca>
2023-10-02 16:23:32 -0400

    added small flavour text after role selection

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ choice = ""
 while True:
     role = input("Please enter 1 for pizza delivery driver, or 2 for mail courier: ")
     
-    if role.isdigit():
+    if role.isdigit(): #checks if role is a number
         role = int(role)
         if role == 1 or role == 2: #checks if role is 1 or 2
             if role == 1: #assigns name
@@ -19,16 +19,16 @@ while True:
             while True:
                 choice = input("Are you sure you want to play as " +name + "? Yes or No: ")
                 if choice == "Yes" or choice == "No":
-                    break
+                    break #exits the confirmation loop
                 else:
                     print("Error, invalid answer.")
 
             if choice == "Yes":
                 print("Great! You have chosen to play as the " +name +"!")
-                break
+                break #exits the role selection loop. Will only activate if role 1 or 2 is selected and "Yes" is selected
         else:
             print("Error, invalid answer.")
     else:
         print("Error, invalid answer.")
 
-print("here")
+print("Let's begin your journey!")
