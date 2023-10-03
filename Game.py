9d8fe64730d1f7ad77cee135d5c2f655aea69d53
Andy Do <doandy@sheridancollege.ca>
2023-10-03 17:09:14 -0400

    Changed Game

--- Game.py
+++ Game.py
@@ -1,1 +1,14 @@
 """This module contains the game logic like methods"""
+import random
+from Role1 import Pizza
+
+def roll_dice():
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+    sum = d1 + d2
+    print("You rolled " + str(sum))   
+    return sum
+
+def stat_change(r, stat, inc):
+    if r == 1:
+        Pizza.mod_stat(stat, inc)
