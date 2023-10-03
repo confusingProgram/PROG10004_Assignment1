042a881eb51c9a7131b4f855e29d49a2deed8593
confusingProgram <146753271+confusingProgram@users.noreply.github.com>
2023-10-03 15:23:02 -0400

    Added roll_dice() method

--- Game.py
+++ Game.py
@@ -1,13 +1,8 @@
-food = "apple"
-food2 = "orange"
-food3 = "spaghetti"
-food4 = "grape"
-food5 = "cheese"
-food6 = "tomato"
-food7 = "pineapple"
-drink1 = "iced tea"
-drink2 = "apple juice"
-drink3 = "cranberry juice"
-drink4 = "lemon juice"
-drink5 = "tea"
-drink6 = "orange juice"#
+import random
+
+def roll_dice():
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+    sum = d1 + d2
+    print("You rolled " + str(sum))
+    return sum
