b4246c5f7f4d3b40d4a3d4fc4fcbf6f61b03d67d
Nuzzy05 <nuzrah200@gmail.com>
2023-10-06 09:04:23 -0400

    challenge 3

--- Game.py
+++ Game.py
@@ -309,7 +309,7 @@ def chapter_2_challenge(c1):
         print()
     print('                                                 *End of Chapter 2*')
 
-#Chapter 1 finished
+#Chapter 1 
 def chapter_1_challenge(c2):
     if c2._role =="mc": #mail courier 
         option = 0
@@ -493,3 +493,110 @@ def chapter_2_challenge(c2):
 print('                                                 *This Ends Chapter 2*')
 
 #Chapter 2 finished
+
+# chapter 3
+    elif c2._role == "mc": # mail courier
+        print('                         *Chapter 3: The Delivery*')
+        print(c2._name + ''': "123 That Street, OH! found it!"''')
+        print(' You get of the vehicle and make your way to get the boxes out. ')
+        print('                                          *SLAM*                                ')
+        print(" As you are apporaching the main door, a dog runs up to you")
+        print(c2._name + ''': "ohh what do we have here?"''')
+        print('''Dog: "*BARK* *WOOF* *WOOF*."''')
+        print('''Even though the dog seems nice, it starts biting your leg"''')
+        print('                                     Objective: Protect the Boxes!')
+        print("wooh, this dog is really chewing you leg down. How are you going to make sure that the boxes are kept safe!")
+        print("You could try to fling the dog off your leg; put the boxes down and bark back; or run away while the dog chases you.")
+
+
+     def chapter_3_challenge(c2):
+         if c2._role == "mc": #mail courier
+        option = ""
+  while True: # Selection loop
+            op1 = "fling the dog off your leg" # requires 8 strength
+            op2 = "put the boxes down and bark back" # requires 7 dexterity to pass
+            op3 = "run away while the dog chases you " # requires 7 intelligence to pass
+            
+            print("Options:") # Displays options
+            print("1 [STR]. " + op1 + ".")
+            print("2 [DEX]. " + op2 + ".")
+            print("3 [IQ]. "  + op3 + ".")
+
+            while True: # Choosing options loop
+                option = input("Type in 1, 2, or 3: ")
+                if option == "1":
+                    op = op1
+                    break # If user selects a valid option, options loop will exit
+                elif option == "2":
+                    op = op2
+                    break
+                elif option == "3":
+                    op = op3
+                    break
+                else:
+                    print("Error, invalid answer.")
+
+            while True: #Confirm loop
+                choice = input("Are you sure you want to " + op + "?  Yes or No: ")
+                if choice == "Yes" or choice == "No":
+                    break # If user says Yes or No, confirm loop will exit
+                else:
+                    print("Error, invalid answer.")
+            
+            if choice == "Yes":
+                print("You have chosen to: " + op + ".")
+                break # If user said yes, selection loop will exit.
+        
+        print("Let's get the challenge started.")
+
+        if option == "1":
+            print("fling the dog off your leg requires a roll of 8 in order to complete the challenge.")
+            input("Press Enter to roll the dice.")
+            num = roll_dice(c2._str)
+            if num > 10: #11-12
+                print("Critical win! With one fell flick, the dog falls down and scurries away.")
+                c2.challenge_result(3, 1)
+                c2.mod_stat(str, 1)
+            elif num > 6: #7-10
+                print("Win! You try to flicking the dog off, but he tired so he leaves.")
+                c2.challenge_result(3, 1)
+            elif num > 3: #4-5
+                print("Lose! The dog is more aggresive and bites you even harder")
+                c2.challenge_result(3, 0)
+            else: #2-3
+                print("Critical lose! The dog's bite so severe that you end up in a hospital.")
+                c2.challenge_result(3, 0)
+        elif option == "2":
+            print("Drop the boxes and bark back at the dog requires a roll of 7 in order to  complete the challenge.")
+            input("Press Enter to roll the dice.")
+            num = roll_dice(c2._dex)
+            if num > 10: #11-12
+                print("Critical win! Your barks intimidates the dog and runs away. Good job!")
+                c2.challenge_result(3, 1)
+            elif num > 6: #5-10
+                print("Win! As you are putting the boxes down, the dog gets scared by the boxes.")
+                c2.challenge_result(3, 1)
+            elif num > 3: #4-5
+                print("Lose! You drop the boxes on the dog by accident and someone see you. ")
+                c2.challenge_result(3, 0)
+            else: #2-3
+                print("Critical lose! You end up slipping on dog poop trying to put the boxes down. *BOO HOO*")
+                c2.challenge_result(3, 0)
+        elif option == "3":
+            print(" Run away while the dog chases you requires a roll of 7 in order to complete the challenge.")
+            input("Press Enter to roll the dice.")
+            num = roll_dice(c2._iq)
+            if num > 10: #11-12
+                print("Critical win! As you are runing away, the dog gets distracted by the sprinkler!")
+                c1.challenge_result(3, 1)
+            elif num > 6: #7-10
+                print("Win! Another dog passes by and the dog goes to it. That's one way to get away.")
+                c1.challenge_result(3, 1)
+            elif num > 3: #4-5
+                print("Lose! You bump into a pole while trying to run away from the dog. That's got to hurt!")
+                c2.challenge_result(3, 0)
+            else: #2-3
+                print("Critical lose! The is still chasing you which cause you to trip and drop the boxes.")
+                c2.challenge_result(3, 0)
+
+    print('                                                 *This Ends Chapter 3*')
